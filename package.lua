30e96036c59f01b45956ff13a769b03d60ad5311
Nils 'Linkie Pi' Reid <nils.reid@neoshadow.co>
2020-09-29 16:27:56 +0200

    Updated package.lua to new PPM.

--- package.lua
+++ package.lua
@@ -1,11 +1,2 @@
 
-return {
-	name = "pancake-lib",
-	author = "Nils 'Linkie Pi' Reid",
-	version = "0.0.1",
-
-	src_dir = "sources/",
-	meta_dir = "sources/",
-	build_cfg = "build.nelua", 
-	dependencies = {}
-}
+ppm.include_sources('sources/')
