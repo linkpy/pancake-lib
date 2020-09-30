c9f6dcf6e244f7e518309a37d986eae4a2244aba
Nils 'Linkie Pi' Reid <nils.reid@neoshadow.co>
2020-09-30 13:14:12 +0200

    Removed the .neluacfg.*.lua.

--- .neluacfg.lua
+++
@@ -1,3 +0,0 @@
-return {
-	add_path={"./sources/"}
-}
--- .neluacfg.ppm.lua
+++
@@ -1,6 +0,0 @@
-return {
-	add_path = {
-		[=[sources/]=],
-	},
-	cache_dir = [=[/home/pancake/Documents/devel/nelua/pancake-lib/nelua_cache]=],
-}
